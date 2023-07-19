233a168218c312f9dd232ca7d083923aff62479c
AminulHaqueShihab <61130539+AminulHaqueShihab@users.noreply.github.com>
2023-07-19 23:54:49 +0600

    Favicon and title added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import Hero from "./components/Hero";
 import HeadlineCards from "./components/HeadlineCards";
 import Food from "./components/Food";
 import Category from "./components/Category";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
+    
     <div className='pt-2'>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>YumCart</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Food delivery website" />
+      </Helmet>
       <Navbar />
       <Hero />
       <HeadlineCards />
